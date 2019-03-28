ede0f8e7bd3d660e2737d5a559ba191c79b3f59e
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-28 16:44:13 -0400

    added default router and mounting points

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,10 @@
 const router = require('express').Router();
+const ingredients = require('./ingredients');
+const dishes = require('./dishes');
+const recipes = require('./recipes');
+
+router.use('/ingredients', ingredients);
+router.use('/dishes', dishes);
+router.use('/recipes', recipes);
 
 module.exports = router;
